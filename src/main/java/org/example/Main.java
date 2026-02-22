073dc01cc1d79dcb41b0d3154d80ef58e9fe0938
SKOTLA01 <saiteja.kotla1@gmail.com>
2026-02-22 18:16:35 +0530

    Added JDBC code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,7 +21,7 @@ public class Main {
     Class.forName("org.postgresql.Driver");
 
     String url="jdbc:postgresql://localhost:5432/Root";
-    String user="postgre";
+    String user="postgres";
     String passwod="0000";
 
     Connection connection = DriverManager.getConnection(url,user,passwod);
