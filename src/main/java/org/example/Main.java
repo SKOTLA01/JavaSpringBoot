5da830198fe5dcd1d18d9de6a05289758f5fa81e
SKOTLA01 <saiteja.kotla1@gmail.com>
2026-02-22 18:02:52 +0530

    Added JDBC code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,34 @@
 package org.example;
+import java.sql.*;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // Download JDBC driver from mvn
+
+        //Connecting java and Database
+
+        /*
+        * Import Package
+        * Load and register
+        * Create connections
+        * execute statement
+        * process results
+        * close
+        * */
+    Class.forName("org.postgresql.Driver");
+
+    String url="jdbc:postgresql://localhost:5432/Root";
+    String user="postgre";
+    String passwod="0000";
+
+    Connection connection = DriverManager.getConnection(url,user,passwod);
+        System.out.printf("Connection establish");
+
+
+
 
     }
 }
